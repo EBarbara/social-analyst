d3ef4a139c9ded8cf2e3607b785c9dfa6ca90972
Estevan Barbará <estevan.barbara@gmail.com>
2017-08-29 07:46:17 -0300

    Sink dando problema

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # initializations
     spark = SparkSession.builder.appName("SocialAnalyst").getOrCreate()
     dimensions = 10
-    folder = "C:\\Users\\Estevan\\PycharmProjects\\Mining\\tweets"
+    folder = "C:/tweets"
     preprocessingModule = PreprocessingModule(inputCol="text", outputCol="words")
     vectorizingModule = VectorizingModule(inputCol="words", outputCol="features", dimensions=dimensions)
 
@@ -18,10 +18,17 @@ if __name__ == "__main__":
     tweets_filtered = preprocessingModule.run(tweets).drop("tokens")
     tweets_vectorized = vectorizingModule.run(tweets_filtered).drop("words")
 
+    # classifying
     nb = NaiveBayes(smoothing=1.0, modelType="multinomial", featuresCol="features")
     nb_model = NaiveBayesModel.load("training_data/trained_naive_bayes_model")
     tweets_classified = nb_model.transform(tweets_vectorized).drop("rawPrediction", "probability", "features")
     tweets_final = tweets_classified.select("*").where("prediction != 0.0")
 
-    query = tweets_final.writeStream.outputMode("append").format("console").start()
+    # sinking
+    query = tweets_final.writeStream.\
+        outputMode("append").\
+        format("json").\
+        option("path", "classified_data").\
+        option("checkpointLocation", "data_checkpoint").\
+        start()
     query.awaitTermination()
